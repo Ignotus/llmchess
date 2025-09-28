959687fa1b1b4ff6f20f3c1367bb61563fb4f5d4
Minh Ngo <nlminhtl@gmail.com>
2025-09-28 12:13:53 +0200

    feat(training): add more target layers for training

--- llmchess/train.py
+++ llmchess/train.py
@@ -65,7 +65,7 @@ def main() -> None:
     lora_config = LoraConfig(
         r=8,
         lora_alpha=16,
-        target_modules=["q_proj", "v_proj"],
+        target_modules=["q_proj", "v_proj", "k_proj", "o_proj"],
         lora_dropout=0.05,
         bias="none",
         task_type="CAUSAL_LM",
