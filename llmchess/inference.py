aee200a8a53ae6022af34541ac6970e070104627
Minh Ngo <nlminhtl@gmail.com>
2025-10-01 22:15:43 +0200

    feat(inference): game simulation

--- llmchess/inference.py
+++ llmchess/inference.py
@@ -1,3 +1,5 @@
+import argparse
+
 from transformers import (
     AutoTokenizer,
 )
@@ -8,12 +10,17 @@ import torch
 
 
 def main() -> None:
-    MODEL_PATH = "./output"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--elo1", default=1500)
+    parser.add_argument("--elo2", default=1400)
+    parser.add_argument("--model-path", default="./output")
+
+    args = parser.parse_args()
 
     model = AutoPeftModelForCausalLM.from_pretrained(
-        MODEL_PATH, torch_dtype=torch.bfloat16
+        args.model_path, torch_dtype=torch.bfloat16
     )
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_path, padding_side="left")
 
     model = model.merge_and_unload()
 
@@ -21,24 +28,33 @@ def main() -> None:
     model.to(device)
     model.eval()
 
-    elo = 1500
-    text = f"Previous Chess Moves: e4 e5 Bc4 Nc6 Nf3 Nf6 c3\nNext Chess Player Elo: {elo}\nNext Chess Move: "
-    print(text)
-    input_ids = tokenizer(text, return_tensors="pt").input_ids.to(device)
+    elo1 = args.elo1
+    elo2 = args.elo2
+    moves = ["e4"]
 
     with torch.no_grad():
-        output_ids = model.generate(
-            input_ids,
-            max_length=512,
-            num_beams=5,
-            do_sample=True,
-        )
+        for i in range(1, 50):
+            moves_prompt = ""
+            if moves:
+                moves_prompt = f"Previous Chess Moves: {' '.join(moves)}\n"
 
-    generated_text = tokenizer.decode(
-        output_ids[0, len(input_ids[0]) :], skip_special_tokens=True
-    )
+            elo = elo1 if i % 2 == 0 else elo2
+            prompt = f"{moves_prompt}Next Chess Player Elo: {elo}\nNext Chess Move: "
+            input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
 
-    print(generated_text)
+            output_ids = model.generate(
+                input_ids,
+                max_length=512,
+                num_beams=5,
+                do_sample=True,
+            )
+
+            next_move = tokenizer.decode(
+                output_ids[0, len(input_ids[0]) :], skip_special_tokens=True
+            ).split("<EOS>", 1)[0]
+
+            moves.append(next_move)
+            print(moves)
 
 
 if __name__ == "__main__":
